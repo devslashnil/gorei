0949f1950e6683253e61160843895e2c467a88fc
Mikhail Potapov <devslashnil@gmail.com>
2022-08-03 12:40:49 +0300

    working code

--- main.go
+++ main.go
@@ -11,13 +11,7 @@ import (
 	"strconv"
 )
 
-// TODO: linters and install all
-// TODO: rewrite all comments
-// TODO: add tests
-// TODO: add linters
-// TODO: add functions as Cloud analogue
-
-// Update is a Tg object that the handler receives every time an user interacts with the bot.
+// Update is a Tg object that the handler receives every time a user interacts with the bot.
 type Update struct {
 	UpdateId int     `json:"update_id"`
 	Message  Message `json:"message"`
@@ -45,17 +39,14 @@ func parseTgRequest(r *http.Request) (*Update, error) {
 }
 
 // HandleTgWebHook sends a message back to the chat with a punchline starting by the message provided by the user.
-func HandleTgWebHook(w http.ResponseWriter, r *http.Request, updHandler func(upd string) string) {
-
+func handleTgWebHook(_ http.ResponseWriter, r *http.Request, updHandler func(upd string) string) {
 	// Parse incoming request
 	upd, err := parseTgRequest(r)
 	if err != nil {
 		log.Printf("error parsing update, %s", err)
 		return
 	}
-
 	responseText := updHandler(upd.Message.Text)
-
 	responseBody, err := sendTextToChat(upd.Message.Chat.Id, responseText)
 	if err != nil {
 		log.Printf("got error %s from telegram, response body is %s", err, responseBody)
@@ -65,9 +56,14 @@ func HandleTgWebHook(w http.ResponseWriter, r *http.Request, updHandler func(upd
 
 }
 
+func CreateWebHookHandler(updHandler func(upd string) string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		handleTgWebHook(w, r, updHandler)
+	}
+}
+
 // sendTextToTelegramChat sends a text message to the Telegram chat identified by its chat id
 func sendTextToChat(id int, text string) (string, error) {
-
 	log.Printf("Sending %s to chat_id: %d", text, id)
 	response, err := http.PostForm(
 		"https://api.telegram.org/bot"+os.Getenv("TELEGRAM_BOT_TOKEN")+"/sendMessage",
@@ -75,13 +71,11 @@ func sendTextToChat(id int, text string) (string, error) {
 			"chat_id": {strconv.Itoa(id)},
 			"text":    {text},
 		})
-
 	if err != nil {
 		log.Printf("error when posting text to thee chat: %s", err)
 		return "", err
 	}
 	defer response.Body.Close()
-
 	bodyBytes, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		log.Printf("error in parsing telegram answer %s", err)
@@ -89,13 +83,17 @@ func sendTextToChat(id int, text string) (string, error) {
 	}
 	bodyString := string(bodyBytes)
 	log.Printf("Body of Telegram Response: %s", bodyString)
-
 	return bodyString, nil
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
-func main() {
 
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/echo", CreateWebHookHandler(func(s string) string {
+		return s
+	}))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
